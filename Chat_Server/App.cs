a06ac73fd49d7bbb9e8c607845c8684976936fbb
Calebe Libério <calebe.liberio@vertown.com>
2023-10-18 20:41:41 -0300

    fix server

--- Chat_Server/App.cs
+++ Chat_Server/App.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
@@ -16,8 +14,7 @@ namespace Chat_Server
 
         static void Main()
         {
-            var enderecoIPLocal = IPAddress.Parse(RecuperarEnderecoIPLocal());
-            listener = new TcpListener(enderecoIPLocal, 6969);
+            listener = new TcpListener(IPAddress.Any, 6969);
             listener.Start();
             Console.WriteLine("Server is running...");
 
@@ -36,39 +33,17 @@ namespace Chat_Server
             using (NetworkStream stream = client.GetStream())
             {
                 byte[] buffer = new byte[1024];
-                int bytesRead;
+                int bytesRead = 0;
 
-                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                while (bytesRead == stream.Read(buffer, 0, buffer.Length))
                 {
-                    string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    string message = Encoding.ASCII.GetString(buffer, 0, buffer.Length);
                     Console.WriteLine("Received from C#: " + message);
 
                     // Encaminhar a mensagem para clientes Java
                     Broadcast(message);
                 }
             }
-        }
-
-        private static string RecuperarEnderecoIPLocal()
-        {
-
-            // Obtém a lista de todas as interfaces de rede da máquina.
-            var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces().ToList();
-
-            // Filtra as interfaces de rede que têm o tipo "Wireless80211"
-            var networkInterface = networkInterfaces.Where(x => x.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 && x.Name == "Wi-Fi").FirstOrDefault();
-
-            if (networkInterface == null)
-                throw new Exception("Erro ao recuperar o endereço IP da márquina.");
-
-
-            // Recupera o endereço IPv4 da rede Wi-Fi
-            var ipv4 = networkInterface.GetIPProperties().UnicastAddresses.Where(x => x.Address.AddressFamily == AddressFamily.InterNetwork).FirstOrDefault();
-
-            if (ipv4 == null)
-                throw new Exception("Endereço IPv4 não encontrado");
-
-            return ipv4.Address.ToString();
         }
 
         static void Broadcast(string message)
